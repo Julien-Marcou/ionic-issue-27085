95f688c4e4a8a686a7fb4030789e53b0ca0fe8e9
Julien Marcou <julien.marcou.dev@gmail.com>
2023-04-10 18:27:53 +0200

    Add reproduction demo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { TranslocoModule } from '@ngneat/transloco';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [TranslocoModule, IonicModule, ReactiveFormsModule],
 })
 export class HomePage {
-  constructor() {}
+  protected readonly form = new FormGroup({
+    options: new FormControl('1'),
+    input1: new FormControl('Value'),
+    input2: new FormControl(null),
+    input3: new FormControl(null),
+  });
 }
